f7465087fb30c5ded4d386e6033afbf4749e12ca
Radik Khaziev <121854191+Croleack@users.noreply.github.com>
2024-04-06 11:48:43 +0500

    Initial Commit

---
+++ testVk/testVkApp.swift
@@ -0,0 +1,17 @@
+//
+//  testVkApp.swift
+//  testVk
+//
+//  Created by Enzhe Gaysina on 06.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct testVkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
